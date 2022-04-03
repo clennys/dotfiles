d1aa87feb508245bd87864ba7edb2b04e8046de6
Dennys <dennys.huber@pm.me>
2022-04-04 00:21:31 +0200

    update

---
+++ roles/X/files/ifi_lab.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --mode 2560x1440 --pos 3840x0 --rotate normal --scale 1.75x1.75 --output HDMI-0 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal
--- roles/scripts/files/utility/startdwm.sh
+++
@@ -1,3 +0,0 @@
-while true; do
-   dbus-launch --exit-with-session dwm 2> ~/.dwm.log
-done
