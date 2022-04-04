52c3ad8a2660a800321713e3fba0ed787544936f
Dennys <dennys.huber@pm.me>
2022-04-05 00:51:43 +0200

    fixed icons symlinks

--- roles/X/files/julianita.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --scale 1.75x1.75 --output HDMI-0 --mode 2560x1440 --pos 4480x0 --rotate normal --scale 1.75x1.75 --output DP-2 --mode 3840x2160 --pos 2646x2520 --rotate normal
